9887fdc5435d5fd7482a8322800a6d7745671ee2
sebas <sebas@DESKTOP-T6U13RC.speedport.ip>
2023-09-16 09:47:12 +0200

    Test

---
+++ Üben/src/Cat.java
@@ -0,0 +1,17 @@
+
+public class Cat {
+private	String name;
+private		int age;
+private		int hp;
+	
+
+
+public String getName() {
+	return name;
+	
+}
+public void setName(String name) {
+	this.name = name;
+	
+}
+}
---
+++ Üben/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+public static void main (String [] args) {
+	Cat myCat  = new Cat();
+	myCat.setName("Basti");
+	System.out.println(myCat.getName());
+	
+	
+	
+	
+}
+}
